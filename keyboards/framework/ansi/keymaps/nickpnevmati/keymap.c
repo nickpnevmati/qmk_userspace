57aa03febdb07d0136d93fc74eaca63ba1dadd88
Nick Pnevmati <nickpnevmati@gmail.com>
2026-07-13 18:17:54 +0300

    Edit Framework keymap

--- keyboards/framework/ansi/keymaps/nickpnevmati/keymap.c
+++ keyboards/framework/ansi/keymaps/nickpnevmati/keymap.c
@@ -4,6 +4,53 @@
 #include QMK_KEYBOARD_H
 #include "framework.h"
 
+
+// ---------- TAP DANCE STUFF ----------
+enum {
+    TD_CAPS_CW = 0,
+};
+
+// ---------- CAPS WORD - CAPS LOCK ----------
+void caps_cw_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1){
+        caps_word_toggle();
+    } else if (state->count >= 2) { 
+        tap_code(KC_CAPS);
+    }
+}
+
+void caps_cw_reset(tap_dance_state_t *state, void *user_data) { }
+
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_CAPS_CW]  = ACTION_TAP_DANCE_FN_ADVANCED(NULL, caps_cw_finished, caps_cw_reset),
+};
+
+// ---------- TAP DANCE STUFF ----------
+
+static bool     lgui_shift_held = false;
+static bool     lgui_tapped     = false;
+static uint16_t lgui_timer      = 0;
+
+bool tap_dance_lgui(keyrecord_t *record) {
+    if (record->event.pressed) {
+        if (lgui_tapped && timer_elapsed(lgui_timer) < TAPPING_TERM) {
+            lgui_shift_held = true;
+            register_mods(MOD_BIT(KC_LSFT));
+        }
+        lgui_tapped = false;
+    } else {
+        if (lgui_shift_held) {
+            unregister_mods(MOD_BIT(KC_LSFT));
+            lgui_shift_held = false;
+        } else {
+            lgui_tapped = true;
+            lgui_timer  = timer_read();
+        }
+    }
+    return true;
+}
+
 enum _layers {
   _BASE,
   _FN,
@@ -32,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * 78 total
      */
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, KC_BRIU, KC_SCRN, KC_NO, KC_PSCR, KC_MSEL, KC_DEL,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_ESC,           KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, KC_BRIU, KC_SCRN, KC_NO, KC_PSCR, KC_MSEL, KC_DEL,
+        KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        TD(TD_CAPS_CW),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
         KC_LCTL, MO(_FN), KC_LGUI, KC_LALT,          KC_SPC,                    KC_RALT, KC_RCTL, KC_LEFT,   KC_UP, KC_DOWN, KC_RGHT
     ),
@@ -99,6 +146,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     set_single_persistent_default_layer(_BASE);
                 }
             }
+            return false;
+            break;
+        case KC_LGUI:
+            tap_dance_lgui(record);
             return false;
             break;
         default:
